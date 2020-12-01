f10a1c7e7b152b5e5baef8e34403a17b6919870e
miyamoto-yoshiki <yo5shi2ki8@yahoo.co.jp>
2020-12-02 01:33:22 +0900

    顧客登録機能、登録顧客の一覧表示

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,25 @@
 class ClientsController < ApplicationController
   def index
+    @allclients = Client.all
+  end
+
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+    if @client.valid?
+      @client.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def client_params
+    params.require(:client).permit(:last_name, :first_name, :last_kana, :first_kana, :email, :phone_num, :birthday, :post_num, :prefecture, :city, :details, :build_num, :post_num).merge(user_id: current_user.id)
   end
 end
---
+++ app/models/client.rb
@@ -0,0 +1,17 @@
+class Client < ApplicationRecord
+  belongs_to :user
+
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' } do
+    validates :last_kana
+    validates :first_kana
+  end
+
+  validates :phone_num,     format: { with: /\A[0-9]+\z/ } , length: { maximum: 11}
+  validates :email, uniqueness: true, presence: true
+  validates :post_num,      format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+end
