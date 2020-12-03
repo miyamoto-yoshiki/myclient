54b8c60ede26f7af2ba744809faf4e0443da985f
miyamoto-yoshiki <yo5shi2ki8@yahoo.co.jp>
2020-12-03 17:04:43 +0900

    編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to:'tops#index'
-  resources :clients, only:[:index, :new, :create, :show]
+  resources :clients, only:[:index, :new, :create, :show, :edit, :update]
 end
