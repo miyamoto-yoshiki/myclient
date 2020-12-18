f0265c5742a03b5c41805b010bcee81ad5099077
miyamoto-yoshiki <yo5shi2ki8@yahoo.co.jp>
2020-12-18 16:51:06 +0900

    DB設計追記

--- db/migrate/20201207084442_create_tasks.rb
+++ db/migrate/20201207084442_create_tasks.rb
@@ -3,8 +3,8 @@ class CreateTasks < ActiveRecord::Migration[6.0]
     create_table :tasks do |t|
       t.string :task, null: false
       t.date :date, null: false
-      t.references :client, null: false, foreign_key: true
       t.integer :redirect
+      t.references :client, null: false, foreign_key: true
       t.timestamps
     end
   end
