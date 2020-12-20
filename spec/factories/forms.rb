759375f71cb50762fca0fc144fb454cece29dfb2
miyamoto-yoshiki <yo5shi2ki8@yahoo.co.jp>
2020-12-20 13:22:42 +0900

    Formモデルテストコード実装

--- spec/factories/forms.rb
+++ spec/factories/forms.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :form do
-    
+    f_name1 { 'テスト' }
+    f_name2 { 'テスト' }
+    f_name3 { 'テスト' }
+    f_name4 { 'テスト' }
+    f_name5 { 'テスト' }
+    f_name6 { 'テスト' }
+    f_name7 { 'テスト' }
+    f_name8 { 'テスト' }
+    f_name9 { 'テスト' }
+    f_name10 { 'テスト' }
+    user_id { 1 }
   end
 end
