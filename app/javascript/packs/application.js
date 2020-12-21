fb1780c6a6ca95fc93299889b2066772a0e48f82
miyamoto-yoshiki <yo5shi2ki8@yahoo.co.jp>
2020-12-22 02:16:51 +0900

    タスクの保存ボタンが登録できるようにした

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 require("@rails/ujs").start()
 // require("@rails/activestorage").start()
 require("channels")
-require("jquery").start()
-require("jquery_ujs").start()
-require("turbolinks").start()
+// require("jquery").start()
+// require("jquery_ujs").start()
+// require("turbolinks").start()
 //= require_tree .
