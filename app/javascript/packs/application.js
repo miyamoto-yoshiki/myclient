d31e79e06e502fa96434657928575ac926c37aa1
miyamoto-yoshiki <yo5shi2ki8@yahoo.co.jp>
2020-12-21 19:09:27 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require jquery
-require jquery_ujs
+// require jquery_ujs
 // require turbolinks
 // require_tree .
 
