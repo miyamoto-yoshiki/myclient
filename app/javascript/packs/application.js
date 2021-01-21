48a6f51870d4b80ab262fa1d3d3a08b18afc75af
miyamoto-yoshiki <yo5shi2ki8@yahoo.co.jp>
2021-01-21 18:32:49 +0900

    顧客削除ボタンにホバー時アラートを表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ require("@rails/ujs").start()
 // require("@rails/activestorage").start()
 require("channels")
 require("../info");
+require("../delete");
 // require("jquery").start()
 // require("jquery_ujs").start()
 // require("turbolinks").start()
